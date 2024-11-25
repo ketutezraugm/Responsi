37635a023d8c3605d59e66e765a39f71982660e5
ketutezraugm <iketutezrahesperosmaska@mail.ugm.ac.id>
2024-11-25 16:01:51 +0700

    Update palindrome.cpp

--- No 5/palindrome.cpp
+++ No 5/palindrome.cpp
@@ -3,14 +3,18 @@
 using namespace std;
 
 int main() {
+    // variables
     string word,reversed;
-    
+
+    // input
     cout << "Enter a string: ";
     getline(cin, word);
-    
+
+    // reverse the string
     for (int i=word.length()-1; i>=0; i--) 
         reversed += word[i];
-    
+
+    // check for palindrome
     if (reversed == word) 
         cout << word << " is a palindrome";
     else
