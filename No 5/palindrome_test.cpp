ad7602ce6bf19775054a0facec040e863886112f
ketutezraugm <iketutezrahesperosmaska@mail.ugm.ac.id>
2024-11-25 16:02:35 +0700

    Update palindrome_test.cpp

--- No 5/palindrome_test.cpp
+++ No 5/palindrome_test.cpp
@@ -3,13 +3,17 @@
 using namespace std;
 
 int main() {
+    // variables
     string word="hellolleh",reversed;
-    
+
+    // reverse the string
     for (int i=word.length()-1; i>=0; i--) 
         reversed += word[i];
-    
+
+    // output the original string
     cout << "String: " << word << endl;
-    
+
+    // check for palindrome
     if (reversed == word) 
         cout << word << " is a palindrome";
     else
