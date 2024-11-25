463e59b8486f0e9a67ca2cd53f1e01a0637ae9b5
ketutezraugm <iketutezrahesperosmaska@mail.ugm.ac.id>
2024-11-25 16:04:29 +0700

    Update reverse_test.cpp

--- No 2/reverse_test.cpp
+++ No 2/reverse_test.cpp
@@ -3,11 +3,14 @@
 using namespace std;
 
 int main() {
+    // variables
     string word="hello world",reversed;
-    
+
+    // reverse the string
     for (int i=word.length()-1; i>=0; i--) 
         reversed += word[i];
-        
+
+    // output the original and reversed string
     cout << "String: " << word << endl;
     cout << "Reversed String: " << reversed;
     
