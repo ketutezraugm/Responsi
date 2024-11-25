3e255988f9ee2aee4b6601de592f027a9f0f4352
ketutezraugm <iketutezrahesperosmaska@mail.ugm.ac.id>
2024-11-25 16:03:58 +0700

    Update reverse.cpp

--- No 2/reverse.cpp
+++ No 2/reverse.cpp
@@ -3,14 +3,18 @@
 using namespace std;
 
 int main() {
+    // variables
     string word,reversed;
-    
+
+    // input
     cout << "Enter a string: ";
     getline(cin, word);
-    
+
+    // reverse the string
     for (int i=word.length()-1; i>=0; i--) 
         reversed += word[i];
-        
+
+    // output the reversed string
     cout << "Reversed String: " << reversed;
     
     return 0;
